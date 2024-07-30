33f554ddf3c37896813ae52d1fd1ebe619e2eabf
Trevor Wong <twong@fleetds.com>
2024-07-30 13:49:47 -0700

    testing deploy

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,13 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Text } from '@mantine/core';
 
 export function HomePage() {
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+      <Text>Hi</Text>
     </>
   );
 }
