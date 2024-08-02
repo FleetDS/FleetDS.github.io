8acf778dc9ea1a766040f5c28e9ce7fe6bac16a4
Trevor Wong <twong@fleetds.com>
2024-08-02 11:27:04 -0700

    update to use hashrouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,23 +1,16 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 import { ContactPage } from './pages/Contact.page';
 import { ProductPage } from './pages/Product.page';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/products',
-    element: <ProductPage />,
-  },
-  {
-    path: '/contact',
-    element: <ContactPage />,
-  },
-]);
-
 export function Router() {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </HashRouter>
+  );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,8 +17,8 @@ export function Header() {
       />
       <nav className={styles.nav}>
         <a href="/" className={styles.navLink}>Home</a>
-        <a href="/products" className={styles.navLink}>Our Products</a>
-        <a href="/contact" className={styles.navLink}>Contact</a>
+        <a href="#/products" className={styles.navLink}>Our Products</a>
+        <a href="#/contact" className={styles.navLink}>Contact</a>
       </nav>
     </div>
   );
