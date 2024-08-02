29a0e249658fa3b5617d5bf8805d97389825170d
Trevor Wong <twong@fleetds.com>
2024-08-02 13:24:34 -0700

    editing header to have a burger on mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
-import { Image} from '@mantine/core';
+import { Burger, Image} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import logo from '../../assets/fdslogo.png';
 import styles from './Header.module.css'; // Ensure this file exists
 
 export function Header() {
-  
+  const [opened, setOpened] = useState(true);
+
   return (
     <div className={styles.headerContainer}>
       <Image
@@ -15,7 +16,12 @@ export function Header() {
         src={logo}
         className={styles.logo}
       />
-      <nav className={styles.nav}>
+      <Burger
+        opened={opened}
+        onClick={() => setOpened((o) => !o)}
+        className={styles.burger}
+      />
+      <nav className={styles.nav} style={{ display: opened ? 'flex' : 'none' }}>
         <a href="/" className={styles.navLink}>Home</a>
         <a href="#/products" className={styles.navLink}>Our Products</a>
         <a href="#/contact" className={styles.navLink}>Contact</a>
