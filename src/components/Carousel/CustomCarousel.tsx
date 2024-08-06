010f02f8bf2afb0d47108b672ea9451d610114d4
Trevor Wong <twong@fleetds.com>
2024-08-06 14:53:00 -0700

    changing banner text and photos

--- src/components/Carousel/CustomCarousel.tsx
+++ src/components/Carousel/CustomCarousel.tsx
@@ -6,41 +6,36 @@ import { useRef } from 'react';
 import Autoplay from 'embla-carousel-autoplay';
 
 import banner1 from '../../assets/banner12to1.jpg';
+import banner2 from '../../assets/banner2.webp';
+import banner3 from '../../assets/banner2.jpg';
+import banner4 from '../../assets/banner3.jpg';
+import banner5 from '../../assets/AFS.jpg';
 
 const data = [
   {
-    image: banner1,
-    title: 'Best forests to visit in North America',
+    image: banner2,
+    title: 'Streamline Your Fleet Operations',
     category: 'nature',
   },
   {
-    image:
-      'https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Hawaii beaches review: better than you think',
+    image: banner1,
+    title: 'Safety First',
     category: 'beach',
   },
   {
-    image:
-      'https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Mountains at night: 12 best locations to enjoy the view',
+    image: banner4,
+    title: 'Leverage Advanced Technology',
     category: 'nature',
   },
   {
-    image:
-      'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Aurora in Norway: when to visit for best experience',
-    category: 'nature',
-  },
-  {
-    image:
-      'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Best places to visit this winter',
+    image: banner5,
+    title: 'Manage Mobile Refueling',
     category: 'tourism',
   },
   {
     image:
-      'https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Active volcanos reviews: travel at your own risk',
+    banner3,
+    title: 'Exceptional Support',
     category: 'nature',
   },
 ];
