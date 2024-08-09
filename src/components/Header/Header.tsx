939097729b0858b08bc151f96a87794443b75539
Trevor Wong <twong@fleetds.com>
2024-08-09 13:07:09 -0700

    updating site to work on mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,21 @@
-import { Burger, Image, Anchor } from '@mantine/core';
-import { useState } from 'react';
-import logo from '../../assets/home/fdslogo.png';
-import logo1 from '../../assets/out-3.jpg'
+import { Image, Anchor } from '@mantine/core';
+import logo1 from '../../assets/out-3.jpg';
 import styles from './Header.module.css';
 
 export function Header() {
-  const [opened, setOpened] = useState(false);
-
   return (
     <div className={styles.headerContainer}>
       <Anchor href="#/" className={styles.logoLink}>
-        <Image h={70} w="auto" radius="md" src={logo1} className={styles.logo} />
+        <Image radius="md" src={logo1} className={styles.logo} />
       </Anchor>
-      <Burger opened={opened} onClick={() => setOpened((o) => !o)} className={styles.burger} />
-      <div className={`${styles.nav} ${opened ? styles.navVisible : styles.navHidden}`}>
-        <Anchor href="/" className={styles.navLink} underline="never" fw={500} fz="xl">
+      <div className={styles.nav}>
+        <Anchor href="/" className={styles.navLink} underline="never" fw={500}>
           Home
         </Anchor>
-        <Anchor href="#/products" className={styles.navLink} underline="never" fw={500} fz="xl">
+        <Anchor href="#/products" className={styles.navLink} underline="never" fw={500}>
           Our Products
         </Anchor>
-        <Anchor href="#/contact" className={styles.navLink} underline="never" fw={500} fz="xl">
+        <Anchor href="#/contact" className={styles.navLink} underline="never" fw={500}>
           Contact
         </Anchor>
       </div>
